a8c5a61105102c456ba8a10e224350aaef7c4b37
Carl Reinke <carlreinke@users.noreply.github.com>
2022-09-08 20:38:11 -0600

    Update file header

--- Tetractic.CodeAnalysis.ExceptionAnalyzers.Analyzers/INamedTypeSymbolExtensions.cs
+++ Tetractic.CodeAnalysis.ExceptionAnalyzers.Analyzers/INamedTypeSymbolExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright 2021 Carl Reinke
 //
-// This file is part of a library that is licensed under the terms of GNU Lesser
-// General Public License version 3 as published by the Free Software
+// This file is part of a library that is licensed under the terms of the GNU
+// Lesser General Public License Version 3 as published by the Free Software
 // Foundation.
 //
 // This license does not grant rights under trademark law for use of any trade
