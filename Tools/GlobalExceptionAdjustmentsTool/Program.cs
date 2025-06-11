9650fbe5db05a81a1416492eb906b2fdefea1695
Carl Reinke <carlreinke@users.noreply.github.com>
2025-06-10 18:50:18 -0600

    Add global adjustments

--- Tools/GlobalExceptionAdjustmentsTool/Program.cs
+++ Tools/GlobalExceptionAdjustmentsTool/Program.cs
@@ -66,8 +66,8 @@ internal static class Program
             [
                 @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.8\",
                 @"C:\Program Files\dotnet\sdk\NuGetFallbackFolder\netstandard.library\2.0.3\build\netstandard2.0\ref",
-                @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.14\ref\net8.0\",
-                @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\9.0.3\ref\net9.0\",
+                @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.16\ref\net8.0\",
+                @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\9.0.5\ref\net9.0\",
             ];
 
             Check(paths);
